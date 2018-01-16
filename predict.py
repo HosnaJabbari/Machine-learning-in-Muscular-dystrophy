473309d9421effbfa6ecd0ff3ffa1dac6680394e
Mahyar Hosseini <mahyar.hosseini@ualberta.ca>
2018-01-16 14:30:36 -0700

    Add a function to be responsible of generating different learner instance

--- predict.py
+++ predict.py
@@ -1,6 +1,8 @@
 import IScore.call_iscore as c_iscore
 import IScore.iscore as iscore
 from sklearn import model_selection, linear_model
+from sklearn.kernel_ridge import KernelRidge
+from sklearn.svm import SVR
 from sklearn.metrics import mean_squared_error, r2_score
 import pandas
 import numpy as np
@@ -8,16 +10,16 @@ import matplotlib.pyplot as plt
 import math
 
 
-def cross_validation(lreg, X_data, y_data, k_fold):
+def cross_validation(learner, X_data, y_data, k_fold):
     # K-FOLD CROSS VALIDATION
     scoring_metrics = ['neg_mean_squared_error']
-    result = model_selection.cross_validate(lreg, X_data, y_data, cv=k_fold, scoring=scoring_metrics)
+    result = model_selection.cross_validate(learner, X_data, y_data, cv=k_fold, scoring=scoring_metrics)
     return result
 
 
 
 # Select best feature subset among the iscore suggested subsets
-def find_best_features(data_frame, target_feature_name, initial_subset_len, k_fold, bins_num, error_range):
+def find_best_features(data_frame, target_feature_name, initial_subset_len, k_fold, bins_num, error_range, learner_name):
     # FEATURE SELECT0ION
     df = c_iscore.convert_normalized_to_discrete_equal_bin(data_frame, bins_num) # I-Score works only with descrete values
     max_subsets = c_iscore.feature_selection(df, target_feature_name, initial_subset_len, bins_num, error_range) # TODO check if the feature selection excludes the target column from the dependant column
@@ -31,7 +33,7 @@ def find_best_features(data_frame, target_feature_name, initial_subset_len, k_fo
 
     # K-FOLD CROSS VALIDATION
 #    scoring_metrics = ['neg_mean_squared_error']
-#   results = model_selection.cross_validate(lreg, X_data, y_data, cv=k, scoring=scoring_metrics)
+#   results = model_selection.cross_validate(learner, X_data, y_data, cv=k, scoring=scoring_metrics)
     y_data = df[target_feature_name.replace('dummy', '')]
     
     min_error = float('Inf')
@@ -42,9 +44,9 @@ def find_best_features(data_frame, target_feature_name, initial_subset_len, k_fo
         X_data = df[cols]
         #print X_data
         
-        lreg = linear_model.LinearRegression() # Create linear regression object
+        learner = get_learner_instance(learner_name) 
         #Internal cross-validation   
-        result = cross_validation(lreg, X_data, y_data, k_fold) 
+        result = cross_validation(learner, X_data, y_data, k_fold) 
         #print(result)
          
 
@@ -69,15 +71,15 @@ def partition(df, num_partition):
     return dfs
 
 
-def learn(df, target_feature_name, feature_subset):
+def learn(df, target_feature_name, feature_subset, learner_name):
     columns = [i.replace('dummy', '') for i in feature_subset]
     y_data = df[target_feature_name.replace('dummy', '')]
     print(columns)
     X_data = df[columns]
-    lreg = linear_model.LinearRegression() # Create linear regression object
+    learner = get_learner_instance(learner_name) 
     # Train the model using the training sets
-    lreg.fit(X_data, y_data)
-    return lreg
+    learner.fit(X_data, y_data)
+    return learner
 
 
 def calculate_accuracy(observed_list, predict_list, thresh_value):
@@ -98,15 +100,37 @@ def calculate_accuracy(observed_list, predict_list, thresh_value):
     return len(observed_yes_predict_yes + observed_no_predict_no)/float(len(observed_list))
 
 
+def get_learner_instance(learner_name): 
 
-def learner(data_frame, target_feature_name, initial_subset_len, k_fold, bins_num, error_range):
+    # Some input parameters are hard coded, you might need to change it
+    if learner_name == 'linear_regression':
+        return linear_model.LinearRegression() # Create linear regression object
+
+    elif learner_name == 'SVR_RBF':
+        return SVR(kernel='rbf', C=1e3, gamma=0.1)
+
+    elif learner_name == 'SVR_linear':
+        return SVR(kernel='linear', C=1e3)
+
+    elif learner_name == 'SVR_polynomial':
+        return SVR(kernel='poly', C=1e3, degree=2)
+
+    elif learner_name == 'kernel_ridge':
+        return KernelRidge(alpha=1.0)
+
+    else: 
+        print('Error: The requested machine learning algorithm is not defined!')
+        return None
+
+
+def get_learning_model(data_frame, target_feature_name, initial_subset_len, k_fold, bins_num, error_range, learner_name):
     # Feature selection
 #    print('old_df', data_frame)
     df = data_frame.copy() # Make a copy of data frame to keep the original data unchanged (since find_best_features first normalizes the data and then discretizes it.)
-    feature_subset, dummy_error = find_best_features(df, target_feature_name, initial_subset_len, k_fold, bins_num, error_range) # This calculated error is not valid, should be replaced with average errors of the crosvalidation in the following
+    feature_subset, dummy_error = find_best_features(df, target_feature_name, initial_subset_len, k_fold, bins_num, error_range, learner_name) # This calculated error is not valid, should be replaced with average errors of the crosvalidation in the following
 #    print('new_df', df)
     # Learning using all data rows as input
-    lreg = learn(data_frame, target_feature_name, feature_subset)
+    learner = learn(data_frame, target_feature_name, feature_subset, learner_name)
 
     # Cross-validation
     partitions = partition(df, k_fold)
@@ -118,14 +142,14 @@ def learner(data_frame, target_feature_name, initial_subset_len, k_fold, bins_nu
         # round, the discritization will be different and the changes reamain in the 
         # data frame (that's why we copy).
         train_df_cp = train_df.copy()
-        temp_feature_subset, dummy_error = find_best_features(train_df_cp, target_feature_name, initial_subset_len, k_fold, bins_num, error_range)
-        tmp_lreg = learn(train_df, target_feature_name, temp_feature_subset)
+        temp_feature_subset, dummy_error = find_best_features(train_df_cp, target_feature_name, initial_subset_len, k_fold, bins_num, error_range, learner_name)
+        tmp_learner = learn(train_df, target_feature_name, temp_feature_subset, learner_name)
         columns = [i.replace('dummy', '') for i in temp_feature_subset]
         test_X_data = test_df[columns]
         test_y_data = test_df[target_feature_name.replace('dummy', '')]
 
         # Predict
-        pred_y_data = tmp_lreg.predict(test_X_data)
+        pred_y_data = tmp_learner.predict(test_X_data)
         # Calculating error: mean_squared_error
         error_sum += mean_squared_error(test_y_data, pred_y_data)
  
@@ -133,12 +157,12 @@ def learner(data_frame, target_feature_name, initial_subset_len, k_fold, bins_nu
     avg_error = float(error_sum)/len(partitions)
 
     # The coefficients
-#    print('Coefficients: \n', lreg.coef_)
+#    print('Coefficients: \n', learner.coef_)
     # The mean squared error
 #    print("Mean squared error: %.2f" %mean_squared_error(diabetes_y_test, diabetes_y_pred))
     # Explained variance score: 1 is perfect prediction
 #    print('Variance score: %.2f' % r2_score(diabetes_y_test, diabetes_y_pred))
-    return lreg, avg_error, [i.replace('dummy', '') for i in feature_subset]
+    return learner, avg_error, [i.replace('dummy', '') for i in feature_subset]
 
 
 if __name__ == '__main__':
@@ -150,29 +174,30 @@ if __name__ == '__main__':
     bins_num = 11 # It is fixed according to convert_normalized_to_discrete function
     error_range = 0.0001
     k_fold = 5
+    learner_name = 'SVR_polynomial'
 
     #Read input
     df = c_iscore.read_file(f_addr)
     #df = c_iscore.convert_normalized_to_discrete_equal_bin(data_frame, bins_num)
 
     # Learn ML model
-    lreg, avg_error, feature_subset = learner(df, target_feature_name, initial_subset_len, k_fold, bins_num, error_range)   
+    learner, avg_error, feature_subset = get_learning_model(df, target_feature_name, initial_subset_len, k_fold, bins_num, error_range, learner_name)   
     
     # Predict
     columns = [i.replace('dummy', '') for i in feature_subset]
     X_data = df[columns]
     observed_y_data = df[target_feature_name.replace('dummy', '')]
-    predict_y_data = lreg.predict(X_data)
+    predict_y_data = learner.predict(X_data)
     thresh_value = 0.3
     
     # Print Information
     print()
-    print('Linear regression model:', lreg)
+    print('Machine learning model:', learner)
     print('Features: ', feature_subset) # TODO check if the order of the features are the same as the coefficients
-    print('Coefficients: ', lreg.coef_)
-    print('Intercept:', lreg.intercept_)
+#    print('Coefficients: ', learner.coef_)
+    print('Intercept:', learner.intercept_)
     print('Mean squared error:%.2f' %avg_error)
-    #print(lreg.get_params(deep=True)) 
+    #print(learner.get_params(deep=True)) 
 
     print('input features:', X_data)  
     print('Observed: ', observed_y_data.as_matrix())
