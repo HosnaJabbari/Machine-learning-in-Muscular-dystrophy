b373556f3b6112d74ac5c29141a2c6c9979bafa5
Mahyar Hosseini <mahyar.hosseini@ualberta.ca>
2018-01-29 12:07:09 -0700

    Add Logger class

---
+++ logger.py
@@ -0,0 +1,18 @@
+class Logger:
+
+    log_file = None
+
+    def __init__(self, path):
+        if Logger.log_file is None:
+            Logger.log_file = open(path, 'w')
+        else:
+            print "A log file is already created named: ", Logger.log_file
+
+    def log(self, message):
+        Logger.log_file.write(message)
+        Logger.log_file.flush()
+
+    def __del__(self):
+        if Logger.log_file is not None:
+            Logger.log_file.flush()
+            Logger.log_file.close()
