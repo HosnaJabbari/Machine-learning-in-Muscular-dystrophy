71e260ee2d9fa5acd0a5a0c833561f2cb48e6cee
Mahyar Hosseini <mahyar.hosseini@ualberta.ca>
2018-02-05 12:46:53 -0700

    Fix Iscore calculation

--- IScore/call_iscore.py
+++ IScore/call_iscore.py
@@ -86,6 +86,7 @@ def convert_normalized_to_discrete_equal_section(data_frame):  # TODO Should be 
 
 
 def get_all_initial_subsets(columns_label_list, subset_len):
+    #return [columns_label_list]
     return set(itertools.combinations(columns_label_list, subset_len))    
 
 
@@ -135,7 +136,7 @@ def partition(df, features_subset, granularity_num):
         if cell_inx not in cells:
             cells[cell_inx] = [row]
         else:
-            cells[cell_inx].append
+            cells[cell_inx].append(row)
     return cells
 
 
@@ -150,115 +151,207 @@ def get_iscore(df, feature_sample, granularity_num, target_feature_name):
         temp = 0
         #temp_counter = 0
         avg = 0
+        elem_count = 0
         for elem in cl:
             temp += getattr(elem, target_feature_name)
-            #temp_counter += 1
+            elem_count += 1
         if len(cl) != 0:
             avg = float(temp)/len(cl)
-        cells_avg[key] = avg
+        cells_avg[key] = (avg, elem_count)
 
     return isc.compute_iscore(target_values, cells_avg)
 
 
-#Backward Dropping Algorithm
+def static_vars(**kwargs):
+    """Defines static variable in a function. Currently, there is no built-in implementation in  Python"""
+
+    def decorate(func):
+        for k in kwargs:
+            setattr(func, k, kwargs[k])
+        return func
+
+    return decorate
+
+# elems_in_range is a list of tuples: (tuple's corresponding value, tuple)
+@static_vars(lower_bound=-float("inf"), elems_in_range=[])
+def keep_max_elements_with_range(new_element, value, error_range):  # Warning: this method has static variables
+    #lower_bound = -float("inf")  # plus inf
+    #upper_bound = float("inf")  # minus inf
+    old_lower_bound = keep_max_elements_with_range.lower_bound
+
+    if value >= keep_max_elements_with_range.lower_bound:
+        # Updating lower and upper bound and add new element
+        if value - error_range > keep_max_elements_with_range.lower_bound:
+            keep_max_elements_with_range.lower_bound = value - error_range
+
+        # Add new element
+        keep_max_elements_with_range.elems_in_range.append((value, new_element))
+
+        # Remove unwanted values based on the new lower bound
+        if old_lower_bound != keep_max_elements_with_range.lower_bound:
+            keep_max_elements_with_range.elems_in_range.sort(key=lambda tup: tup[0])  # sorts in place based on the value
+            for indx in xrange(len(keep_max_elements_with_range.elems_in_range)):
+                tmp_value, arr = keep_max_elements_with_range.elems_in_range[indx]
+                if tmp_value >= keep_max_elements_with_range.lower_bound:
+                    break  # Since the array is sorted the rest are greater than the lower bound
+
+            del keep_max_elements_with_range.elems_in_range[:indx]
+    median_value = keep_max_elements_with_range.lower_bound + error_range
+    return keep_max_elements_with_range.elems_in_range, median_value
+
+
+# elems_in_range_container is a list of tuples: (tuple's corresponding value, tuple)
+def pick_max_elements_within_range(new_element, value, error_range, lower_bound=-float("inf"), elems_in_range_container=[]):
+    #lower_bound = -float("inf")  # plus inf
+    #upper_bound = float("inf")  # minus inf
+    old_lower_bound = lower_bound
+
+    if value >= lower_bound:
+        # Updating lower and upper bound and add new element
+        if value - error_range > lower_bound:
+            lower_bound = value - error_range
+
+        # Add new element
+            elems_in_range_container.append((value, new_element))
+
+        # Remove unwanted values based on the new lower bound
+        if old_lower_bound != lower_bound:
+            elems_in_range_container.sort(key=lambda tup: tup[0])  # sorts in place based on the value
+            for indx in xrange(len(elems_in_range_container)):
+                tmp_value, arr = elems_in_range_container[indx]
+                if tmp_value >= lower_bound:
+                    break  # Since the array is sorted the rest are greater than the lower bound
+
+            del elems_in_range_container[:indx]
+
+    return elems_in_range_container, lower_bound
+
+
+def sample_uniformly(samples_list, num):
+
+    length = len(samples_list)
+    # step = length//num
+    step = num
+
+    # if step == 0:
+    if length <= step:
+        return samples_list
+
+    selected_samples = []
+    for i in xrange(0, length, step):
+        selected_samples.append(samples_list[i])
+
+    return selected_samples
+
+
+def is_exist(my_dict, current_element):
+    key = len(current_element)
+    return (key in my_dict and current_element in my_dict[key])
+
+
+# Backward Dropping Algorithm
 def BDA(df, initial_features_sample, granularity_num, target_feature_name, error_range):
     #global_max_iscore = -float('Inf')
     #global_max_subset = []
 
-    global_max_iscore = get_iscore(df, initial_features_sample, granularity_num, target_feature_name)
+    #global_max_iscore = get_iscore(df, initial_features_sample, granularity_num, target_feature_name)
 
-    global_max_subset = [initial_features_sample]
-    sample_star = initial_features_sample
-    
+    # global_max_subset = [initial_features_sample]
+    global_max_subset = []
+    global_max_iscore = 0
+    all_sample_stars = [(global_max_iscore, initial_features_sample)]
+
 #    print "\n---------------------------------------------- BDA started --------------------------------------------\n"
-    while len(sample_star) > 1: 
-#        print "\nSTART LOCAL #####################################################"
+
+    while len(all_sample_stars) > 0 and len(all_sample_stars[0][1]) > 1:
         local_max_iscore = -float('Inf')
         local_max_subset = []
-        for i in range(len(sample_star)):
-##            print type([sample_star])
-##            print 'sample_star: ', sample_star
-##            print 'first: ', sample_star[:i]
-##            print 'second: ', sample_star[i+1:]
-##            print list(sample_star[:i]) + list(sample_star[i+1:])
-            local_sample = list(sample_star[:i]) + list(sample_star[i+1:]) 
-            #Compute I-Score
-            iscore = get_iscore(df, local_sample, granularity_num, target_feature_name)
-             
-#            print '\n', iscore, 'local_sample: len(',len(local_sample), ')', local_sample
-            if abs(iscore - local_max_iscore) <= error_range:
-                local_max_subset.append(local_sample)
-            #elif iscore - local_max_iscore > error_range:
-            else:
-                local_max_iscore = iscore
-                local_max_subset = [local_sample]
-#                print '\n------------------------------------\n', local_max_iscore, 'local_max:', local_max_subset, '\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n'
+        temp_max_iscore = 0
+        tmp_max_set = []
+        last_candidates = []
+        global_dict = {}
 
-        #Drop a variable
-        sample_star = local_max_subset[-1]
+        for sample_star in all_sample_stars:
+#        print "\nSTART LOCAL #####################################################"
+            local_max_iscore = 0
+
+            tmp_lower_bound = -float("inf")
+            tmp_elems_in_range_container = []
+            count_child = 0
+            for i in range(len(sample_star[1])):
+    ##            print type([sample_star])
+    ##            print 'sample_star: ', sample_star
+    ##            print 'first: ', sample_star[:i]
+    ##            print 'second: ', sample_star[i+1:]
+    ##            print list(sample_star[:i]) + list(sample_star[i+1:])
+                local_sample = list(sample_star[1][:i]) + list(sample_star[1][i+1:])
+                #Compute I-Score
+                if is_exist(global_dict, local_sample):
+                    continue
+                iscore = get_iscore(df, local_sample, granularity_num, target_feature_name)
+
+
+
+                #tmp_elems_in_range_container, tmp_lower_bound = pick_max_elements_within_range(local_sample, iscore, error_range, tmp_lower_bound, tmp_elems_in_range_container)
+                global_max_subset, global_max_iscore = keep_max_elements_with_range(local_sample, iscore, error_range)  # This method has static variables
+
+
+                # if abs(iscore - global_max_iscore) >= error_range:
+                #     step = len(sample_star[1]) / 2
+                #     if count_child < 2 or count_child % step == 0:
+                #         last_candidates.append((iscore, local_sample))
+                #     count_child += 1
+                #     print 'last candidate len: ', len(last_candidates)
+                # if abs(iscore - local_max_iscore) <= error_range or local_max_iscore == 0:
+                if iscore >= local_max_iscore:
+                    key = len(local_sample)
+                    local_max_iscore = iscore
+                    if not is_exist(global_dict, local_sample):
+                        if key in global_dict:
+                            global_dict[key].append(local_sample)
+                        else:
+                            global_dict[key] = [local_sample]
+
+                        # last_candidates = [(iscore, local_sample)]
+                    # if local_max_iscore == iscore:
+                        last_candidates.append((iscore, local_sample))
+                        print 'Last candidate len: ', len(last_candidates)
+                        if 'Binding_percentage_Hfold_interacting' in local_sample or 'Delta_Delta_G' in local_sample:
+                            print "iscore and local sample", iscore, 'len:', len(local_sample), local_sample
+
+
+
+
+
+                        # #            print '\n', iscore, 'local_sample: len(',len(local_sample), ')', local_sample
+    #             if abs(iscore - local_max_iscore) <= error_range:
+    #                 local_max_subset.append((iscore, local_sample))
+    #             elif iscore - local_max_iscore > error_range:
+    #             # else:
+    #                 local_max_iscore = iscore
+    #                 local_max_subset = [(iscore, local_sample)]
+    # #                print '\n------------------------------------\n', local_max_iscore, 'local_max:', local_max_subset, '\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n'
+
+        #Drop a variable (i.e., each sample has length of one less than the original)
+        # sample_star = local_max_subset[-1][1]
+        # sample_star = local_max_subset
+        # all_sample_stars = sample_uniformly(last_candidates, 10)
+        all_sample_stars = last_candidates
+
        
-        #Keep the best I-Score
-        print "local_max_iscore: ", local_max_iscore, "global_max_iscore: ", global_max_iscore, "difference: ", abs(local_max_iscore - global_max_iscore)
-        if abs(local_max_iscore - global_max_iscore) <= error_range:
-            global_max_subset += local_max_subset
-        elif local_max_iscore - global_max_iscore > error_range:
-            global_max_iscore = local_max_iscore
-            global_max_subset = local_max_subset
-#            print "\n\n>>>>>>>>>>>>>>>>>>>> Global max updated <<<<<<<<<<<<<<<<<<<<"
-#            print '\n', global_max_iscore, 'local_sample: len(', len(global_max_subset), ')', global_max_subset
-##        print 'global_max_subset', global_max_subset
+#         #Keep the best I-Score
+#         print "local_max_iscore: ", local_max_iscore, "global_max_iscore: ", global_max_iscore, "difference: ", abs(local_max_iscore - global_max_iscore)
+#         if abs(local_max_iscore - global_max_iscore) <= error_range:
+#             global_max_subset += local_max_subset
+#         elif local_max_iscore - global_max_iscore > error_range:
+#             global_max_iscore = local_max_iscore
+#             global_max_subset = local_max_subset
+# #            print "\n\n>>>>>>>>>>>>>>>>>>>> Global max updated <<<<<<<<<<<<<<<<<<<<"
+# #            print '\n', global_max_iscore, 'local_sample: len(', len(global_max_subset), ')', global_max_subset
+# ##        print 'global_max_subset', global_max_subset
+# #     print 'global_max_iscore: ', global_max_iscore, ' global_max_subset: ', global_max_subset
     return global_max_iscore, global_max_subset
 
-
-#Backward Dropping Algorithm
-#def BDA(df, initial_features_sample, granularity_num, target_feature_name):
-#    #global_max_iscore = -float('Inf')
-#    #global_max_subset = []
-#    global_max_iscore = get_iscore(df, initial_features_sample, granularity_num, target_feature_name)
-#    global_max_subset = [initial_features_sample]
-#    sample_star = initial_features_sample
-#    
-#    print 'before: ', initial_features_sample
-#
-#    while len(sample_star) > 1: 
-#        local_max_iscore = -float('Inf')
-#        local_max_subset = []
-#        print 'local_max_subset1: ', local_max_subset
-#        for i in range(len(sample_star)):
-#            feature_sample = sample_star[:i] + sample_star[i+1:] 
-#            #Compute I-Score
-#            iscore = get_iscore(df, feature_sample, granularity_num, target_feature_name)
-#             
-#            if iscore > local_max_iscore:
-#                local_max_iscore = iscore
-#                local_max_subset = [feature_sample]
-#                print 'local_max_subset4: ', local_max_subset, '[feature_sample]: ', [feature_sample]
-#            if iscore == local_max_iscore:
-#                local_max_subset = local_max_subset.append(feature_sample)
-#                print 'local_max_subset5: ', local_max_subset
-#            print 'local_max_subset2: ', local_max_subset
-#
-#        print 'local_max_subset3: ', local_max_subset
-#        #Drop a variable
-#        sample_star = local_max_subset
-#       
-#        #Keep the best I-Score
-#        if local_max_iscore > global_max_iscore:
-#            global_max_iscore = local_max_iscore
-#            global_max_subset = local_max_subset
-#        if local_max_iscore == global_max_iscore:
-#            global_max_subset.append(local_max_subset)
-##        print 'global_max_subset', global_max_subset
-#    return global_max_iscore, global_max_subset
-
-#def add_max(max_list, vector):
-#    if len(max_list) > 0:
-#        if max_list[0] < value:
-#            max_list = [value]
-#        if max_list[0] == value:
-#           max_list.append(value)
-#    return max_list
-     
 
 def feature_selection(data_frame, target_feature_name, initial_subset_len, bins_num, error_range, debug=False):
     max_iscore = -float('Inf')
@@ -327,25 +420,85 @@ def feature_selection(data_frame, target_feature_name, initial_subset_len, bins_
 
 
 if __name__ == '__main__':
-    f_addr = '/home/seyedmah/Desktop/normalized_data_Oct25.xlsx'
+    f_addr = '/home/seyedmah/Desktop/normalized_data_Jan10(Exon_Malueka_Category_C-0_A-1).xlsx'
     target_feature_name = 'skip_percentage'
-    initial_subset_len = 54
+    initial_subset_len = 55
     bins_num = 11#It is fixed according to convert_normalized_to_discrete function
-    error_range = 0.0001
-   
+    error_range = 0.000000000000001
+
     data_frame = read_file(f_addr)
     max_subsets = feature_selection(data_frame, target_feature_name, initial_subset_len, bins_num, error_range)
- 
+
     for i in range(len(max_subsets)):
-        print '\n', i + 1, ' Best feature set len: ', len(max_subsets[i])
+        print '\n', i + 1, ' Best feature set len: ', len(max_subsets[i][1])
         print 'Best feature set: ', max_subsets[i]
 
+    # error_range = 0.5
+    # print keep_max_elements_with_range([20], 20, error_range)
+    # print keep_max_elements_with_range([19.6], 19.6, error_range)
+    # print keep_max_elements_with_range([19.7], 19.7, error_range)
+    # print keep_max_elements_with_range([19.9], 19.9, error_range)
+    # print keep_max_elements_with_range([20.4], 20.4, error_range)
+    # print keep_max_elements_with_range([25], 25, error_range)
+    # print keep_max_elements_with_range([24.5], 24.5, error_range)
+    # print keep_max_elements_with_range([25.5], 25.5, error_range)
+    # print keep_max_elements_with_range([25.3], 25.3, error_range)
 
-
-
-
-
-
-
-
-
+#########################3kevin stuff ########################################3
+#
+# # Backward Dropping Algorithm
+# def BDA(df, initial_features_sample, granularity_num, target_feature_name, error_range):
+#
+#     global_max_subset = []
+#     global_max_iscore = 0
+#     all_sample_stars = [(global_max_iscore, initial_features_sample)]
+#
+#     while len(all_sample_stars) > 0 and len(all_sample_stars[0][1]) > 1:
+#
+#         last_candidates = []
+#
+#         for sample_star in all_sample_stars:
+#             for i in range(len(sample_star[1])):
+#                 local_sample = list(sample_star[1][:i]) + list(sample_star[1][i + 1:])
+#                 # Compute I-Score
+#                 iscore = get_iscore(df, local_sample, granularity_num, target_feature_name)
+#
+#                 if 'Binding_percentage_Hfold_interacting' in local_sample or 'Delta_Delta_G' in local_sample:
+#                     print "iscore and local sample", iscore, 'len:', len(local_sample), local_sample
+#
+#                 # tmp_elems_in_range_container, tmp_lower_bound = pick_max_elements_within_range(local_sample, iscore, error_range, tmp_lower_bound, tmp_elems_in_range_container)
+#                 global_max_subset, global_max_iscore = keep_max_elements_with_range(local_sample, iscore,
+#                                                                                     error_range)  # This method has static variables
+#                 if abs(iscore - global_max_iscore) >= error_range:
+#                     last_candidates.append((iscore, local_sample))
+#
+#         all_sample_stars = sample_uniformly(last_candidates, 5)
+#
+#     return global_max_iscore, global_max_subset
+#
+# # elems_in_range is a list of tuples: (tuple's corresponding value, tuple)
+# @static_vars(lower_bound=-float("inf"), elems_in_range=[])
+# def keep_max_elements_with_range(new_element, value, error_range):  # Warning: this method has static variables
+#     #lower_bound = -float("inf")  # plus inf
+#     #upper_bound = float("inf")  # minus inf
+#     old_lower_bound = keep_max_elements_with_range.lower_bound
+#
+#     if value >= keep_max_elements_with_range.lower_bound:
+#         # Updating lower and upper bound and add new element
+#         if value - error_range > keep_max_elements_with_range.lower_bound:
+#             keep_max_elements_with_range.lower_bound = value - error_range
+#
+#         # Add new element
+#         keep_max_elements_with_range.elems_in_range.append((value, new_element))
+#
+#         # Remove unwanted values based on the new lower bound
+#         if old_lower_bound != keep_max_elements_with_range.lower_bound:
+#             keep_max_elements_with_range.elems_in_range.sort(key=lambda tup: tup[0])  # sorts in place based on the value
+#             for indx in xrange(len(keep_max_elements_with_range.elems_in_range)):
+#                 tmp_value, arr = keep_max_elements_with_range.elems_in_range[indx]
+#                 if tmp_value >= keep_max_elements_with_range.lower_bound:
+#                     break  # Since the array is sorted the rest are greater than the lower bound
+#
+#             del keep_max_elements_with_range.elems_in_range[:indx]
+#     median_value = keep_max_elements_with_range.lower_bound + error_range
+#     return keep_max_elements_with_range.elems_in_range, median_value
